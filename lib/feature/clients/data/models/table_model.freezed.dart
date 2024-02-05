592410ddd85ef7e7ca352fe367b91ee666618c50
IslomjonCoder <106533499+IslomjonCoder@users.noreply.github.com>
2024-02-05 10:02:20 +0500

    menu, product screens are finished

---
+++ lib/feature/clients/data/models/table_model.freezed.dart
@@ -0,0 +1,300 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'table_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+TableModel _$TableModelFromJson(Map<String, dynamic> json) {
+  return _TableModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$TableModel {
+  @JsonKey(name: "_id")
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(name: "stol_nomi")
+  String get stolNomi => throw _privateConstructorUsedError;
+  @JsonKey(name: "QR")
+  String get qr => throw _privateConstructorUsedError;
+  @JsonKey(name: "active")
+  bool get active => throw _privateConstructorUsedError;
+  @JsonKey(name: "kafe_id")
+  String get kafeId => throw _privateConstructorUsedError;
+  @JsonKey(name: "zone_id")
+  String get zoneId => throw _privateConstructorUsedError;
+  @JsonKey(name: "ofisiant_id")
+  String get ofisiantId => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $TableModelCopyWith<TableModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TableModelCopyWith<$Res> {
+  factory $TableModelCopyWith(
+          TableModel value, $Res Function(TableModel) then) =
+      _$TableModelCopyWithImpl<$Res, TableModel>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: "_id") String id,
+      @JsonKey(name: "stol_nomi") String stolNomi,
+      @JsonKey(name: "QR") String qr,
+      @JsonKey(name: "active") bool active,
+      @JsonKey(name: "kafe_id") String kafeId,
+      @JsonKey(name: "zone_id") String zoneId,
+      @JsonKey(name: "ofisiant_id") String ofisiantId});
+}
+
+/// @nodoc
+class _$TableModelCopyWithImpl<$Res, $Val extends TableModel>
+    implements $TableModelCopyWith<$Res> {
+  _$TableModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? stolNomi = null,
+    Object? qr = null,
+    Object? active = null,
+    Object? kafeId = null,
+    Object? zoneId = null,
+    Object? ofisiantId = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      stolNomi: null == stolNomi
+          ? _value.stolNomi
+          : stolNomi // ignore: cast_nullable_to_non_nullable
+              as String,
+      qr: null == qr
+          ? _value.qr
+          : qr // ignore: cast_nullable_to_non_nullable
+              as String,
+      active: null == active
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as bool,
+      kafeId: null == kafeId
+          ? _value.kafeId
+          : kafeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      zoneId: null == zoneId
+          ? _value.zoneId
+          : zoneId // ignore: cast_nullable_to_non_nullable
+              as String,
+      ofisiantId: null == ofisiantId
+          ? _value.ofisiantId
+          : ofisiantId // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$TableModelImplCopyWith<$Res>
+    implements $TableModelCopyWith<$Res> {
+  factory _$$TableModelImplCopyWith(
+          _$TableModelImpl value, $Res Function(_$TableModelImpl) then) =
+      __$$TableModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: "_id") String id,
+      @JsonKey(name: "stol_nomi") String stolNomi,
+      @JsonKey(name: "QR") String qr,
+      @JsonKey(name: "active") bool active,
+      @JsonKey(name: "kafe_id") String kafeId,
+      @JsonKey(name: "zone_id") String zoneId,
+      @JsonKey(name: "ofisiant_id") String ofisiantId});
+}
+
+/// @nodoc
+class __$$TableModelImplCopyWithImpl<$Res>
+    extends _$TableModelCopyWithImpl<$Res, _$TableModelImpl>
+    implements _$$TableModelImplCopyWith<$Res> {
+  __$$TableModelImplCopyWithImpl(
+      _$TableModelImpl _value, $Res Function(_$TableModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? stolNomi = null,
+    Object? qr = null,
+    Object? active = null,
+    Object? kafeId = null,
+    Object? zoneId = null,
+    Object? ofisiantId = null,
+  }) {
+    return _then(_$TableModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      stolNomi: null == stolNomi
+          ? _value.stolNomi
+          : stolNomi // ignore: cast_nullable_to_non_nullable
+              as String,
+      qr: null == qr
+          ? _value.qr
+          : qr // ignore: cast_nullable_to_non_nullable
+              as String,
+      active: null == active
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as bool,
+      kafeId: null == kafeId
+          ? _value.kafeId
+          : kafeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      zoneId: null == zoneId
+          ? _value.zoneId
+          : zoneId // ignore: cast_nullable_to_non_nullable
+              as String,
+      ofisiantId: null == ofisiantId
+          ? _value.ofisiantId
+          : ofisiantId // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$TableModelImpl implements _TableModel {
+  const _$TableModelImpl(
+      {@JsonKey(name: "_id") required this.id,
+      @JsonKey(name: "stol_nomi") required this.stolNomi,
+      @JsonKey(name: "QR") required this.qr,
+      @JsonKey(name: "active") required this.active,
+      @JsonKey(name: "kafe_id") required this.kafeId,
+      @JsonKey(name: "zone_id") required this.zoneId,
+      @JsonKey(name: "ofisiant_id") required this.ofisiantId});
+
+  factory _$TableModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$TableModelImplFromJson(json);
+
+  @override
+  @JsonKey(name: "_id")
+  final String id;
+  @override
+  @JsonKey(name: "stol_nomi")
+  final String stolNomi;
+  @override
+  @JsonKey(name: "QR")
+  final String qr;
+  @override
+  @JsonKey(name: "active")
+  final bool active;
+  @override
+  @JsonKey(name: "kafe_id")
+  final String kafeId;
+  @override
+  @JsonKey(name: "zone_id")
+  final String zoneId;
+  @override
+  @JsonKey(name: "ofisiant_id")
+  final String ofisiantId;
+
+  @override
+  String toString() {
+    return 'TableModel(id: $id, stolNomi: $stolNomi, qr: $qr, active: $active, kafeId: $kafeId, zoneId: $zoneId, ofisiantId: $ofisiantId)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TableModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.stolNomi, stolNomi) ||
+                other.stolNomi == stolNomi) &&
+            (identical(other.qr, qr) || other.qr == qr) &&
+            (identical(other.active, active) || other.active == active) &&
+            (identical(other.kafeId, kafeId) || other.kafeId == kafeId) &&
+            (identical(other.zoneId, zoneId) || other.zoneId == zoneId) &&
+            (identical(other.ofisiantId, ofisiantId) ||
+                other.ofisiantId == ofisiantId));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, id, stolNomi, qr, active, kafeId, zoneId, ofisiantId);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$TableModelImplCopyWith<_$TableModelImpl> get copyWith =>
+      __$$TableModelImplCopyWithImpl<_$TableModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$TableModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _TableModel implements TableModel {
+  const factory _TableModel(
+          {@JsonKey(name: "_id") required final String id,
+          @JsonKey(name: "stol_nomi") required final String stolNomi,
+          @JsonKey(name: "QR") required final String qr,
+          @JsonKey(name: "active") required final bool active,
+          @JsonKey(name: "kafe_id") required final String kafeId,
+          @JsonKey(name: "zone_id") required final String zoneId,
+          @JsonKey(name: "ofisiant_id") required final String ofisiantId}) =
+      _$TableModelImpl;
+
+  factory _TableModel.fromJson(Map<String, dynamic> json) =
+      _$TableModelImpl.fromJson;
+
+  @override
+  @JsonKey(name: "_id")
+  String get id;
+  @override
+  @JsonKey(name: "stol_nomi")
+  String get stolNomi;
+  @override
+  @JsonKey(name: "QR")
+  String get qr;
+  @override
+  @JsonKey(name: "active")
+  bool get active;
+  @override
+  @JsonKey(name: "kafe_id")
+  String get kafeId;
+  @override
+  @JsonKey(name: "zone_id")
+  String get zoneId;
+  @override
+  @JsonKey(name: "ofisiant_id")
+  String get ofisiantId;
+  @override
+  @JsonKey(ignore: true)
+  _$$TableModelImplCopyWith<_$TableModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
